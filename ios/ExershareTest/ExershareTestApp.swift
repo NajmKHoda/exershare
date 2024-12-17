7e1a9feaa1d6cacc8e9c1f6141cf546077c1991c
Najm Hoda <najmkhoda@gmail.com>
2024-12-17 15:14:58 -0800

    Initial commit

---
+++ ios/ExershareTest/ExershareTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExershareTestApp.swift
+//  ExershareTest
+//
+//  Created by Najm Hoda on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ExershareTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
