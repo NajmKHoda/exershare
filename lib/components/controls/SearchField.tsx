86e01748576e69bfdf6249fdd859d75bd767b30c
Najm Hoda <najmkhoda@gmail.com>
2025-02-09 21:24:56 -0800

    Add focus color change for SearchField

--- lib/components/controls/SearchField.tsx
+++ lib/components/controls/SearchField.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useThemeColors } from '@/lib/hooks/useThemeColors';
 import { SymbolView } from 'expo-symbols';
 import { StyleSheet, TextInput, View } from 'react-native';
@@ -9,16 +10,27 @@ interface Props {
 
 export default function SearchField({ value, onChange }: Props) {
     const colors = useThemeColors();
+    const [isFocused, setIsFocused] = useState(false);
 
     return (
-        <View style={ styles.container }>
-            <SymbolView name='magnifyingglass' size={ 24 } weight='semibold' />
+        <View style={[
+            styles.container,
+            { borderBottomColor: isFocused ? colors.accent : colors.primary }
+        ]}>
+            <SymbolView
+                name='magnifyingglass'
+                size={ 24 }
+                weight='semibold'
+                tintColor={ (isFocused ? colors.accent : colors.primary) as string }
+                />
             <TextInput
                 style={{ color: colors.primary, ...styles.input }}
                 placeholder='Search...'
                 value={ value }
                 onChangeText={ onChange }
-                />
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+            />
         </View>
     );
 }
@@ -27,16 +39,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'row',
-        alignItems: 'stretch',
-        paddingBottom: 6,
+        alignItems: 'flex-end',
         gap: 5,
+        paddingBottom: 5,
         borderBottomWidth: 3,
         overflow: 'hidden'
     },
 
     input: {
         fontSize: 20,
-        lineHeight: 20,
-        padding: 0
+        lineHeight: 24,
+        paddingVertical: 0
     }
-})
+});
