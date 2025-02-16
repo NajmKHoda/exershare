f9d7a178006a54a3e166d4c78accb1f84079f64e
Najm Hoda <najmkhoda@gmail.com>
2025-02-16 12:46:54 -0800

    Fix exercise entry deletion in workout modal

--- lib/components/modals/WorkoutModal.tsx
+++ lib/components/modals/WorkoutModal.tsx
@@ -61,17 +61,23 @@ export default function WorkoutModal({ workout, visible, onClose }: Props) {
         onClose();
     }
 
-    const renderItem = ({ item, drag }: RenderItemParams<DataItem>) => {
+    const renderItem = ({ item, drag, getIndex }: RenderItemParams<DataItem>) => {
+        const index = getIndex();
+        if (index === undefined) return null;
+
         return (
             <View style={[ styles.exerciseContainer, { backgroundColor: colors.backgroundSecondary } ]}>
                 <Pressable onLongPress={ drag } style={ styles.exerciseDragHandle }>
                     <SymbolView name='chevron.up.chevron.down' size={ 24 } tintColor={ colors.primary as string } />
                     <ThemeText style={ styles.exerciseText }>{ item.name }</ThemeText>
                 </Pressable>
-                <Pressable onPress={() => setCurWorkout({ 
-                    ...curWorkout, 
-                    exercises: curWorkout.exercises.filter(e => e.id !== item.id)
-                })}>
+                <Pressable onPress={() => {
+                    console.log(`foo; ${index}`);
+                    setCurWorkout({ 
+                        ...curWorkout, 
+                        exercises: curWorkout.exercises.filter((_, i) => i !== index)
+                    });
+                }}>
                     <SymbolView name='xmark.circle.fill' size={ 24 } tintColor={ colors.red as string } />
                 </Pressable>
             </View>
